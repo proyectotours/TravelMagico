ce96100b3901f4d9e437ee741f7e233a340a5ef0
TravelMagico <geminis.jcgc@gmail.com>
2021-11-17 20:35:56 -0600

    modal

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // java script del menu
+/*
 jQuery('document').ready(function(e){
 let btn = $('#toggle');
 let menu = $('.navigation');
@@ -9,8 +10,35 @@ btn.click(function(){
   }else{
     menu.addClass('show');
   }
-})
-
-
-
 });
+});*/
+
+
+let btn = document.querySelector('#toggle');
+let menu = document.querySelector('.navigation');
+
+btn.addEventListener('click', function(e){
+  menu.classList.toggle('show');
+});
+
+window.addEventListener('click', function(e){
+  if(menu.classList.contains('show')&& e.target != menu && e.target != btn){
+    menu.classList.toggle('show');
+  }
+});
+
+let abrir = document.querySelector('.cta');
+let contModal = document.querySelector('.content__modal');
+let cerrar = document.querySelector('#close');
+
+abrir.addEventListener('click', function(){
+  contModal.style.display = 'flex';
+})
+cerrar.addEventListener('click', function(){
+  contModal.style.display = 'none';
+})
+window.addEventListener('click', function(e){
+  if(e.target == contModal){
+    contModal.style.display = 'none';
+  }
+})
